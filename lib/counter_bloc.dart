57010fec2a0b4789bd8798fadea4d9dd17f22335
Akilan <fidelisakilan@gmail.com>
2020-07-11 21:19:18 +0530

    first commit

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:bloc_demo/counter_event.dart';
+
+class CounterBloc {
+  int _counter = 0;
+
+  final _counterStateController = StreamController<int>();
+
+  StreamSink<int> get _inCounter => _counterStateController.sink;
+
+  Stream<int> get counter => _counterStateController.stream;
+
+  final _counterEventController = StreamController<CounterEvent>();
+
+  StreamSink<CounterEvent> get counterEventSink => _counterEventController.sink;
+
+  CounterBloc() {
+    _counterEventController.stream.listen(_mapEventToState);
+  }
+  void _mapEventToState(CounterEvent event) {
+    if (event is IncrementCounter) {
+      _counter++;
+    } else {
+      _counter--;
+    }
+    _inCounter.add(_counter);
+  }
+
+  void dispose() {}
+}
